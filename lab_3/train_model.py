549d9994d3d27e4ab811cd8c81aef92c6394fee4
Gardrak <72844016+Gardrak@users.noreply.github.com>
2025-04-21 02:22:15 +0500

    Update train_model.py

--- lab_3/train_model.py
+++ lab_3/train_model.py
@@ -77,6 +77,4 @@ if __name__ == "__main__":
     dfruns = mlflow.search_runs()
     path2model = dfruns.sort_values("metrics.r2", ascending=False).iloc[0]['artifact_uri'].replace("file://","") + '/model' #путь до эксперимента с лучшей моделью
     print(path2model)
-    text_file = open("best_model.txt", "w")
-    text_file.write(path2model)
-    text_file.close()
+
